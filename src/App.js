5f51c2e2a38e16efed085a7733ee56ec78febd45
AngelaMoesch123 <angelarosesnyder08@gmail.com>
2022-08-09 12:45:35 -0700

    Added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was created by Angela Moesch and is{" "}
+        <a
+          href="https://github.com/AngelaMoesch123/react_weather_app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
