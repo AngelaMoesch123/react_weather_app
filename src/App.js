109a891ae82b42252790b496ebc08e91c0977b15
AngelaMoesch123 <angelarosesnyder08@gmail.com>
2022-08-09 13:08:37 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was created by Angela Moesch and is{" "}
-        <a
-          href="https://github.com/AngelaMoesch123/react_weather_app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was created by Angela Moesch and is{" "}
+          <a
+            href="https://github.com/AngelaMoesch123/react_weather_app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
